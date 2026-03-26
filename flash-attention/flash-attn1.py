85cfac5db76bbf6af4b1b374369d04e5f015bdc5
fjs <fanjinshan_scu@163.com>
2026-03-26 11:38:34 +0800

    add flash-attn1

--- flash-attention/flash-attn1.py
+++ flash-attention/flash-attn1.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class flash_attn1(nn.Module):
-    def __init__(self, Q, K, V):
+    def __init__(self, Q):
         super().__init__()
         self.Tr, self.Tc = 4, 4     # Block Num
         self.seq_len = Q.size(0)
@@ -11,26 +11,25 @@ class flash_attn1(nn.Module):
         self.Br, self.Bc = self.seq_len // self.Tr, self.seq_len // self.Tc     # Block Size
     
     def forward(self, Q, K, V):
-        cur_max = torch.tensor([float('-inf')] * self.seq_len) # 每一行的最大值
-        cur_sum = torch.tensor([0] * self.seq_len) # 每一行的当前和
-        O = torch.full((self.seq_len, self.seq_len), float('-inf'))
-        for i in range(self.Bc):    # outer loop: K,V
-            Ki, Vi = K[i*self.Bc:(i+1)*self.Bc,:], V[i*self.Bc:(i+1)*self.Bc,:]  # from HBM load KV chunk
-            for j in range(self.Br):    # inner loop: Q, O
-                Qj,Oj = Q[j*self.Br:(j+1)*self.Br,:], O[j*self.Br:(j+1)*self.Br,i*self.Bc:(i+1)*self.Bc,:]       # from HBM load QO chunk
-                m_j, l_j = cur_max[i * self.Br:(i+1) * self.Br], cur_sum[i * self.Br:(i+1) * self.Br]   # from HBM load m,j chunk
-                Sij = Qj @ Ki.transpose(-2,-1)      # (Br,h)*(h,Bc) = (Br,Bc)
-                row_max, row_sum = Sij.max(dim=-1).values, Sij.sum(dim=-1)
-                Pij = torch.exp(Sij - row_max)      # (Br,Bc)
-                new_max = max(old_max, row_max)
-                new_sum = old_sum * torch.exp(old_max - new_max) + row_sum * torch.exp(row_max - new_max)
-
-                cur_max[i * self.Br:(i+1) * self.Br], cur_sum[i * self.Br:(i+1) * self.Br] = new_max, new_sum
-
-
-
-
-                
+        O = torch.zeros((self.seq_len, self.hidden_size))
+        cur_max = torch.full((self.seq_len,), float('-inf')) # 每一行的最大值
+        cur_sum = torch.zeros((self.seq_len,)) # 每一行的当前和
+        for j in range(self.Tc):    # outer loop: K,V
+            Kj, Vj = K[j * self.Bc:(j + 1) * self.Bc,:], V[j * self.Bc:(j + 1) * self.Bc,:]  # from HBM load KV chunk
+            for i in range(self.Tr):    # inner loop: Q, O
+                Qi, Oi = Q[i * self.Br:(i + 1) * self.Br,:], O[i * self.Br:(i + 1) * self.Br,:]       # from HBM load QO chunk
+                m_i, l_i = cur_max[i * self.Br:(i + 1) * self.Br], cur_sum[i * self.Br:(i + 1) * self.Br]   # from HBM load m,j chunk
+                Sij = torch.matmul(Qi, Kj.transpose(-2,-1))      # (Br,h)*(h,Bc) = (Br,Bc)
+                m_ij = Sij.max(dim=-1).values      # (Br,)
+                Pij = torch.exp(Sij - m_ij.unsqueeze(-1))      # (Br, Bc) - (Br, 1) = (Br,Bc)
+                l_ij = Pij.sum(dim=-1)           # (Br,)
+                m_i_new = torch.max(m_i, m_ij)     # (Br,)
+                l_i_new = torch.exp(m_i - m_i_new) * l_i + torch.exp(m_ij - m_i_new) * l_ij     # (Br,)
+                Oi = (l_i / l_i_new).unsqueeze(-1) * torch.exp(m_i-m_i_new).unsqueeze(-1) * Oi + \
+                    (torch.exp(m_ij-m_i_new)).unsqueeze(-1) * torch.matmul(Pij, Vj) / l_i_new.unsqueeze(-1)
+                O[i * self.Br:(i + 1) * self.Br,:] = Oi   # write Oj to HBM
+                cur_max[i * self.Br:(i + 1) * self.Br], cur_sum[i * self.Br:(i + 1) * self.Br] = m_i_new, l_i_new     # write m_j, l_j to HBM
+        return O             
 
                 
 
@@ -43,6 +42,6 @@ S = torch.matmul(Q, K.transpose(-2, -1))
 S = F.softmax(S, dim=-1)
 output = torch.matmul(S, V)
 
-flash_attn = flash_attn1()
+flash_attn = flash_attn1(Q)
 output1 = flash_attn(Q, K, V)
-assert torch.allclose(output, output1)
+print(f"Max difference: {torch.max(torch.abs(output - output1))}")
